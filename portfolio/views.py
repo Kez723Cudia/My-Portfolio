dbae7d76defc69ee4e381186975bd9479da7ea30
Captain <kmcudia@student.hau.edu.ph>
2026-06-28 15:01:54 +0800

    Other pages added

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'portfolio/home.html')
 
+def about(request):
+    return render(request, 'portfolio/about.html')
+
+def projects(request):
+    return render(request, 'portfolio/projects.html')
+
+def contact(request):
+    return render(request, 'portfolio/contact.html')
+
 # Create your views here.
